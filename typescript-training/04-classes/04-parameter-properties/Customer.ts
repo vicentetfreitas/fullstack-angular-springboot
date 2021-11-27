154fbfc430c677590a062d0470967d113cfefded
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-26 22:57:08 -0300

    Parameter properties

---
+++ typescript-training/04-classes/04-parameter-properties/Customer.ts
@@ -0,0 +1,24 @@
+class Customer {
+  // created constructor
+  constructor(private _firstName: string, private _lastName: string) {}
+
+  public get firstName(): string {
+    return this._firstName;
+  }
+  public set firstName(value: string) {
+    this._firstName = value;
+  }
+
+  public get lastName() {
+    return this._lastName;
+  }
+  public set lastName(value: string) {
+    this._lastName = value;
+  }
+}
+
+//let´s create an instance
+let myCustomer = new Customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
