9415fd779665fe3d39622fe9c111ab4ace6dae51
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-20 18:41:45 -0300

    Defining Variables - Write Some Code - 1.0

--- typescript-training/02-variables/sample-types.js
+++ typescript-training/02-variables/sample-types.js
@@ -1,8 +1,37 @@
+/**
+ * Basic Type
+ * > boolean: true/false values
+ * > number: Supports integer and floating point numbers
+ * > string: Text date. Enclosed in single or double quotes
+ * > any: Supports "any" datatype assignment
+ *
+ * Define Variables
+ * > let <variableName>: <type> = <initial value>
+ *
+ * TypeScript: "let" keyword
+ * > var: not used
+ * > let: new used
+ *
+ * TypeScript is Strongly Typed
+ *
+ * Type: any
+ * > we can assign different values of any type
+ *
+ * Display Output
+ * >
+ *
+ * Run the App
+ * >
+ *
+ * Template Strings (``)
+ * > console.log(`Hi ${firstName} + " " + ${lastName}`);
+ *
+ */
 // Input
-var found = true;
-var grade = 88.6;
-var firstName = "Anup";
-var lastName = "Kumar";
+var found = true; //true or false
+var grade = 88.6; // numbers floating point or decimal
+var firstName = "Anup"; //String double quotes
+var lastName = 'Kumar'; //String single quotes
 //Output
 console.log(found);
 console.log("The grade is " + grade);
@@ -11,8 +40,5 @@ console.log("Hi " + firstName + " " + lastName);
 // found = 0;
 // grade = "A";
 // firstName = false;
-console.log(found);
-console.log("The grade is " + grade);
-console.log("Hi " + firstName + " " + lastName);
 // use template string
 console.log("Hi " + firstName + " " + lastName);
--- typescript-training/02-variables/sample-types.ts
+++ typescript-training/02-variables/sample-types.ts
@@ -1,8 +1,37 @@
+/**
+ * Basic Type
+ * > boolean: true/false values
+ * > number: Supports integer and floating point numbers
+ * > string: Text date. Enclosed in single or double quotes
+ * > any: Supports "any" datatype assignment
+ *
+ * Define Variables
+ * > let <variableName>: <type> = <initial value>
+ *
+ * TypeScript: "let" keyword
+ * > var: not used
+ * > let: new used
+ *
+ * TypeScript is Strongly Typed
+ *
+ * Type: any
+ * > we can assign different values of any type
+ *
+ * Display Output
+ * >
+ *
+ * Run the App
+ * >
+ *
+ * Template Strings (``)
+ * > console.log(`Hi ${firstName} + " " + ${lastName}`);
+ *
+ */
 // Input
-let found: boolean = true;
-let grade: number = 88.6;
-let firstName: string = "Anup";
-let lastName: string = "Kumar";
+let found: boolean = true; //true or false
+let grade: number = 88.6; // numbers floating point or decimal
+let firstName: string = "Anup"; //String double quotes
+let lastName: string = 'Kumar'; //String single quotes
 
 //Output
 console.log(found);
@@ -13,10 +42,6 @@ console.log("Hi " + firstName + " " + lastName);
 // found = 0;
 // grade = "A";
 // firstName = false;
-
-console.log(found);
-console.log("The grade is " + grade);
-console.log("Hi " + firstName + " " + lastName);
 
 // use template string
 console.log(`Hi ${firstName} ${lastName}`);
