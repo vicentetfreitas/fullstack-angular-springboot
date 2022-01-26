49441b2d80f62950f9b0130139dd109d2bd53c28
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-25 22:49:29 -0300

    Inheritance - Adding Shapes to an Array

---
+++ typescript-training/05-inheritance/ArrayDriver.ts
@@ -0,0 +1,19 @@
+import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
+import { Shape } from "./Shape";
+
+let myShape = new Shape(10, 15);
+let myCircle = new Circle(5, 10, 20);
+let myRectangle = new Rectangle(0, 0, 3, 7);
+
+// declare an array of shapes ... initially empty
+let theShapes: Shape[] = [];
+
+// add the shapes to the array
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes) {
+  console.log(tempShape.getInfo());
+}
