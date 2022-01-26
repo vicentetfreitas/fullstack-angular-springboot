22a40e51c068e0c628496806a85d5ceffe10c61a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-25 23:10:07 -0300

    Abstract Classes - Write Some Code

---
+++ typescript-training/06-abstract-classes/Rectangle.ts
@@ -0,0 +1,31 @@
+import { Shape } from "./Shape";
+
+export class Rectangle extends Shape {
+  
+  constructor(
+    theX: number,
+    theY: number,
+    private _width: number,
+    private _length: number
+  ) {
+    super(theX, theY);
+  }
+   get width(): number {
+    return this._width;
+  }
+   set width(value: number) {
+    this._width = value;
+  }
+   get length() {
+    return this._length;
+  }
+   set length(value: number) {
+    this._length = value;
+  }
+  getInfo(): string {
+      return super.getInfo() + `, width=${this._width}, length=${this._length}`;
+  }
+  calculateArea(): number {
+    return this._width * this._length;
+  }
+}
