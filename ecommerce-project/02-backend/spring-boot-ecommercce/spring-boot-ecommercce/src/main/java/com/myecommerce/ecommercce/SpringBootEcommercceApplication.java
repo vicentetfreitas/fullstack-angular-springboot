46622973f24b4bef76ec24b15e6eebf95f23a1b9
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-27 23:24:44 -0300

    Create Spring Boot Starter Project

---
+++ ecommerce-project/02-backend/spring-boot-ecommercce/spring-boot-ecommercce/src/main/java/com/myecommerce/ecommercce/SpringBootEcommercceApplication.java
@@ -0,0 +1,13 @@
+package com.myecommerce.ecommercce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommercceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommercceApplication.class, args);
+	}
+
+}
---
+++ ecommerce-project/02-backend/spring-boot-ecommercce/spring-boot-ecommercce/src/test/java/com/myecommerce/ecommercce/SpringBootEcommercceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myecommerce.ecommercce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommercceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
