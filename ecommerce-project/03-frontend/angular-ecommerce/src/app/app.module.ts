9481f9c79b0c3a84ee465998ff3d122bf25d6e52
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-29 17:41:11 -0300

    Angular Project - Create Product class and Product Service - Part 1

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -1,18 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ProductService } from 'src/app/services/product.service';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
