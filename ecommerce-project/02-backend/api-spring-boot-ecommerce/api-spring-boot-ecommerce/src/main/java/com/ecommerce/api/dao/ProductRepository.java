2946a640b335f2604e4aad569f5fb34599746f87
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-01-29 20:52:07 -0300

    Add @CrossOrigin

--- ecommerce-project/02-backend/api-spring-boot-ecommerce/api-spring-boot-ecommerce/src/main/java/com/ecommerce/api/dao/ProductRepository.java
+++ ecommerce-project/02-backend/api-spring-boot-ecommerce/api-spring-boot-ecommerce/src/main/java/com/ecommerce/api/dao/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.ecommerce.api.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.api.entity.Product;
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
